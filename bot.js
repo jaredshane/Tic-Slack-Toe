8a1fa865833e606dc659c3428ab070af8150a94f
Wisbell <westleyisbell@gmail.com>
2017-03-23 20:27:28 -0500

    Bot now accepts the command and posts to the general channel the second word in the sentence

--- bot.js
+++ bot.js
@@ -15,7 +15,7 @@ bot.on('start', function() {
 
     // define channel, where bot exist. You can adjust it there https://my.slack.com/services
 
-    bot.postMessageToChannel('general', 'Hey Humans!  I\m back!', params);
+    // bot.postMessageToChannel('general', 'Hey Humans!  I\m back!', params);
 
     // define existing username instead of 'user_name'
     // bot.postMessageToUser('westley', 'Hello Bozo, testing!', params);
@@ -38,7 +38,7 @@ bot.on('start', function() {
 
 bot.on('message', (event) => {
     // console.log('message event fired')
-    // console.log(event)
+    console.log(event)
 
 
     // check if user is logging in to slack
@@ -46,12 +46,25 @@ bot.on('message', (event) => {
         sayHelloOnArrival(event.user)
     }
 
+
+    // check if event is a message and is in general chat
+    if(event.type === "message" && event.channel === "C4N0D2CBB") {
+        console.log("general channel message fired")
+
+        // check messages to see if it is a bot command
+        let commandObj = checkMessageForBotCommand(event.text)
+
+        if(commandObj) {
+            processBotCommand(commandObj)
+        }
+    }
+
 })
 
 
 const sayHelloOnArrival = (userId) => {
 
-    console.log("say hello func called")
+    // console.log("say hello func called")
 
     let params = {
         icon_emoji: ':robot_face:'
@@ -59,4 +72,45 @@ const sayHelloOnArrival = (userId) => {
 
     // use postMessage to send messages using user IDs
     bot.postMessage(userId, 'Hello Bozo!', params);
+}
+
+
+const checkMessageForBotCommand = (channelText) => {
+    // console.log("text", channelText)
+
+    let commandObj = {
+        type: null,
+        argument: null
+    }
+
+    let channelTextArray = channelText.split(' ')
+
+    let command = channelText.split(' ')[0]
+
+    let argument = channelTextArray.slice(1, channelTextArray.length).join(' ');
+
+    // console.log("checkForCommand", channelTextArray[0])
+    // console.log("argument", argument)
+
+    if(command[0] === "!") {
+        commandObj.type = command.slice(1);
+        commandObj.argument = argument;
+
+        // console.log("command", commandObj)
+
+        return commandObj
+    } else {
+        return false
+    }
+}
+
+
+const processBotCommand = (command) => {
+    console.log("command", command)
+
+    const commandList = {
+        "say": bot.postTo('general', command.argument)
+    }
+
+    return commandList[command.type]
 }
